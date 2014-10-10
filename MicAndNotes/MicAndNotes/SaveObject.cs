9c5a607f0c72f3be9a1d5fdb451084b7e3bbfbe5
cusmonaut <vodtech@hotmail.com>
2014-10-09 19:51:45 -0500

    Refactoring classes into different files
    Refactoring classes into different files

---
+++ MicAndNotes/MicAndNotes/SaveObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MicAndNotes
+{
+    [Serializable]
+    internal class SaveObject
+    {
+        public readonly long RecordingDuration;
+        public readonly string RecordingFilename;
+        public readonly List<TimeNote> SectionOccurances;
+        public readonly string TheNote;
+
+
+        public SaveObject(long duration, List<TimeNote> occurances, string note, string recordingname)
+        {
+            RecordingDuration = duration;
+            SectionOccurances = occurances;
+            TheNote = note;
+            RecordingFilename = recordingname;
+        }
+    }
+}
